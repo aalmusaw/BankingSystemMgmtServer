39d8a8d7045d766cabce117c5d617198cc2adbc9
Ali Al-Musawi <almuali699@gmail.com>
2020-12-28 23:05:51 -0500

    combined 2 databses

---
+++ routes/user.js
@@ -0,0 +1,3 @@
+const bcrypt = require('bcrypt');
+const mongoose = require('')
+const route = require('express').Router();
--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const authDb = require('./connections/authDatabase');
-const bankingDb = require('./connections/bankingDatabase');
 const authenticate = require('./middleware/authenticate');
+
+const DB_CONFIG = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false 
+};
+
+mongoose.connect(process.env.DB_CONNECTION_URI, DB_CONFIG, err => {
+    if (err) throw err;
+    else {
+        console.log('Successfully connected to database.');
+    }
+});
 
 const app = express();
 
